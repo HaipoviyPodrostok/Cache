818602f8cd70d4a718a34b06bc8e37c609150570
HaipoviyPodrostok <alexkurteev228@mail.ru>
2025-10-07 00:36:25 +0300

    increased productivity of ideal cache

--- include/ideal_cache.hpp
+++ include/ideal_cache.hpp
@@ -1,19 +1,19 @@
 #pragma once
 
-#include <algorithm>
 #include <cstddef>
 #include <unordered_map>
 #include <vector>
+#include <unordered_set>
 
 namespace IdealCache {
 
 template <typename KeyT = int>
 class IdealCache {
 public:
-    IdealCache(size_t size, size_t num_elems, 
-                        std::vector<KeyT>& input_data);
+    IdealCache(size_t& size, size_t& num_elems, 
+               std::vector<KeyT>& input_data);
     
-    bool lookup_update(KeyT key, size_t cur_step);
+    bool lookup_update(const KeyT& key, const size_t cur_step);
     void make_next_use_map();
 
 private:
@@ -21,14 +21,15 @@ private:
     size_t num_elems_;
     
     std::vector<KeyT> input_data_;
-    std::vector<KeyT> cache_;
-    std::unordered_map<KeyT, std::vector<size_t>> next_use_map_;
     
+    std::unordered_set<KeyT> cache_;
+    std::unordered_map<KeyT, std::vector<size_t>> next_use_map_;
+    std::unordered_map<KeyT, size_t> next_use_index_;
     void push_out_unpopular(size_t );
 };
 
 template <typename  KeyT>
-IdealCache<KeyT>::IdealCache(size_t size, size_t num_elems, 
+IdealCache<KeyT>::IdealCache(size_t& size, size_t& num_elems, 
                              std::vector<KeyT>& input_data)
     : size_(size), 
       num_elems_(num_elems),  
@@ -36,66 +37,70 @@ IdealCache<KeyT>::IdealCache(size_t size, size_t num_elems,
       cache_() {
         cache_.reserve(size_);
         make_next_use_map();
-      }
+}
 
 template <typename KeyT>
 void IdealCache<KeyT>::make_next_use_map() {
     next_use_map_.clear();
     next_use_map_.reserve(num_elems_);
+    next_use_index_.clear();
     
     for (size_t it = 0; it < num_elems_; ++it) {
         auto key = input_data_[it];
         next_use_map_[key].push_back(it);
     }
+    
+    for (auto& [key, _] : next_use_map_) {
+        next_use_index_[key] = 0;
+    }
 }
 
 template <typename KeyT>
-bool IdealCache<KeyT>::lookup_update(KeyT key, size_t cur_step) {
-    if (std::find(cache_.begin(), cache_.end(), key) != cache_.end()) {
+bool IdealCache<KeyT>::lookup_update(const KeyT& key, const size_t cur_step) {
+    if (cache_.find(key) != cache_.end()) {
         return true;
     }
-    else {
-        if (cache_.size() >= size_) {
-            IdealCache<KeyT>::push_out_unpopular(cur_step);
-        }
-        cache_.push_back(key);
+    if (cache_.size() >= size_) {
+        IdealCache<KeyT>::push_out_unpopular(cur_step);
     }
+    cache_.insert(key);
     return false;
 }
 
 template <typename KeyT>
-void IdealCache<KeyT>::push_out_unpopular(size_t cur_step) {
-    auto victim_it = cache_.begin(); 
-    
-    if (victim_it == cache_.end()) { return; }
-    
+void IdealCache<KeyT>::push_out_unpopular(size_t cur_step) {    
+    if (cache_.empty()) { return; }
+
+    KeyT victim_key{};
     size_t latest_usage = 0;
 
-    for (auto it = cache_.begin(); it != cache_.end(); ++it) {
-        auto next_use_it = next_use_map_.find(*it);
+    for(auto& key : cache_) {
+        auto next_use_it = next_use_map_.find(key);
 
         if (next_use_it == next_use_map_.end()) {
-            victim_it = it;
+            victim_key = key;
             break;
-        } else {
-            auto& access_times = next_use_it->second;
-            while (!access_times.empty() && access_times.front() <= cur_step) {
-                access_times.erase(access_times.begin());
-            }
-            
-            if (access_times.empty()) {
-                victim_it = it;
-                break;
-            }
-            
-            size_t next_usage = next_use_it->second.front();
-            
-            if (next_usage > latest_usage) {
-                latest_usage = next_usage;
-                victim_it = it;
-            }
+        }
+        
+        std::vector<size_t>& access_times = next_use_it->second;
+        size_t& idx = next_use_index_[key];
+
+        while (idx < access_times.size() && access_times[idx] <= cur_step) {
+            ++idx;
+        }
+        
+        if (idx == access_times.size()) {
+            victim_key = key;
+            break;
+        }
+
+        size_t next_usage = access_times[idx];
+        if (next_usage > latest_usage) {
+            latest_usage = next_usage;
+            victim_key = key;
         }
     }
-    cache_.erase(victim_it);
+    
+    cache_.erase(victim_key);
 }
 } //namespace IdealCache    
